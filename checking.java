f1375123fe0b3ca6ee04b0eefb70c3f6eb5e152e
DanielShepelev <94698616+DanielShepelev@users.noreply.github.com>
2023-12-26 18:27:19 +0200

    Update2

--- checking.java
+++ checking.java
@@ -1,5 +1,25 @@
-public class SimpleApp {
-    public static void main(String[] args) {
-        System.out.println("Hello from here!");
+import java.io.IOException;
+import java.io.OutputStream;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+public class HelloWorld {
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create();
+        server.bind(null, 8080);
+        server.createContext("/", new MyHandler());
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "Hello, checking!";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
     }
 }
